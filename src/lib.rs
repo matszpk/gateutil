f30fa5d7fcb2dacc92a1d6f62161a53437faab39
matszpk <matszpk@interia.pl>
2023-09-20 09:05:53 +0200

    Next testcase.

--- src/lib.rs
+++ src/lib.rs
@@ -1669,6 +1669,72 @@ mod tests {
         }
 
         // testcase
+        // do not join clause - with one literal clause - shared clause
+        for tv in 0..16 {
+            let mut input_len = 3;
+            let t = (tv & 1) != 0;
+            let t1 = (tv & 2) != 0;
+            let t2 = (tv & 4) != 0;
+            let t3 = (tv & 8) != 0;
+            let mut clauses = vec![
+                (Clause::new_and([(0, false), (1, false)]), t ^ t1 ^ t2 ^ t3),
+                (Clause::new_and([]), false),
+                (Clause::new_xor([(3, t2), (4, false)]), t3),
+                (Clause::new_and([(2, false), (5, t)]), false),
+                (Clause::new_xor([(3, t2)]), t3),
+                (Clause::new_and([(1, false), (7, t), (6, true)]), false),
+            ];
+            let outputs = [(8, false)];
+            let mut output_map = [
+                OutputEntryN::NewIndex(0, false),
+                OutputEntryN::NewIndex(1, false),
+                OutputEntryN::NewIndex(2, false),
+                OutputEntryN::NewIndex(3, t1),
+                OutputEntryN::NewIndex(4, false),
+                OutputEntryN::NewIndex(5, false),
+                OutputEntryN::NewIndex(6, false),
+                OutputEntryN::NewIndex(7, false),
+                OutputEntryN::NewIndex(8, false),
+            ];
+            assert!(join_and_remove_clauses(
+                &mut input_len,
+                &mut clauses,
+                &outputs,
+                &mut output_map
+            ));
+            assert_eq!(3, input_len);
+            assert_eq!(
+                vec![
+                    (Clause::new_and([(0, false), (1, false)]), t ^ t1 ^ t2 ^ t3),
+                    (Clause::new_and([(2, false), (3, t ^ t2 ^ t3)]), false),
+                    (
+                        Clause::new_and([(1, false), (3, t ^ t2 ^ t3), (4, true)]),
+                        false
+                    ),
+                ],
+                clauses,
+                "{}",
+                tv
+            );
+            assert_eq!(
+                [
+                    OutputEntryN::NewIndex(0, false),
+                    OutputEntryN::NewIndex(1, false),
+                    OutputEntryN::NewIndex(2, false),
+                    OutputEntryN::NewIndex(3, t1),
+                    OutputEntryN::Value(false),
+                    OutputEntryN::NewIndex(3, t1 ^ t2 ^ t3),
+                    OutputEntryN::NewIndex(4, false),
+                    OutputEntryN::NewIndex(3, t1 ^ t2 ^ t3),
+                    OutputEntryN::NewIndex(5, false),
+                ],
+                output_map,
+                "{}",
+                tv
+            );
+        }
+
+        // testcase
         // do not join clause - with one literal clause - some literal is false
         for tv in 0..16 {
             let mut input_len = 3;
