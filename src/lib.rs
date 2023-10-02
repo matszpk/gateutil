8073562df840ed50609f02f781144864a4f14f8f
matszpk <matszpk@interia.pl>
2023-10-02 12:49:58 +0200

    Next testcase for join_deduplicates_to_clause_circuit.

--- src/lib.rs
+++ src/lib.rs
@@ -889,5 +889,55 @@ mod tests {
                 &[(6, false), (7, false)]
             )
         );
+        assert_eq!(
+            ClauseCircuit::new(
+                4,
+                [
+                    Clause::new_and([(0, false), (1, true)]),
+                    Clause::new_and([(0, false), (3, true), (4, false)]),
+                    Clause::new_and([(0, false), (5, false)]),
+                    Clause::new_xor([(0, false), (3, true)]),
+                    Clause::new_xor([(0, false), (2, true), (7, true)]),
+                    Clause::new_xor([(2, true), (8, true)]),
+                    Clause::new_and([(1, true), (2, true), (4, false), (6, false)]),
+                    Clause::new_xor([(1, true), (3, true), (7, false), (9, false)]),
+                ],
+                [(10, false), (11, false)]
+            )
+            .unwrap(),
+            join_deduplicates_to_clause_circuit(
+                4,
+                8,
+                vec![
+                    (4, None, Clause::new_and([(0, false), (1, true)])),
+                    (
+                        4,
+                        Some(8),
+                        Clause::new_and([(0, false), (3, true), (4, false)])
+                    ),
+                    (4, Some(10), Clause::new_and([(0, false), (8, false)])),
+                    (
+                        6,
+                        None,
+                        Clause::new_and([(1, true), (2, true), (4, false), (10, false)])
+                    ),
+                ],
+                vec![
+                    (5, None, Clause::new_xor([(0, false), (3, true)])),
+                    (
+                        5,
+                        Some(9),
+                        Clause::new_xor([(0, false), (2, true), (5, true)])
+                    ),
+                    (5, Some(11), Clause::new_xor([(2, true), (9, true)])),
+                    (
+                        7,
+                        None,
+                        Clause::new_xor([(1, true), (3, true), (5, false), (11, false)])
+                    ),
+                ],
+                &[(6, false), (7, false)]
+            )
+        );
     }
 }
