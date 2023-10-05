e7355c255fe058a7f1a2aa5c549ff98cef35fdca
matszpk <matszpk@interia.pl>
2023-10-05 16:52:41 +0200

    Next testcase for deduplicate_literal_clauses_0.

--- src/lib.rs
+++ src/lib.rs
@@ -2284,5 +2284,124 @@ mod tests {
             ],
             clauses,
         );
+
+        let mut clauses = vec![
+            dedup_clause(
+                10,
+                None,
+                Clause::new_and([
+                    (1, false), // 1 (c0, c3, c4)
+                    (2, false),
+                    (5, false), // 1 (c0, c3, c4)
+                ]),
+            ),
+            dedup_clause(
+                11,
+                None,
+                Clause::new_and([
+                    (0, false), // 3 (c1, c3)
+                    (2, false),
+                    (3, false), // 2 (c1, c2, c3)
+                    (4, false), // 3 (c1, c3)
+                    (6, false), // 2 (c1, c2, c3)
+                    (7, false), // 2 (c1, c2, c3)
+                ]),
+            ),
+            dedup_clause(
+                12,
+                None,
+                Clause::new_and([
+                    (1, false), // 1 (c0, c3, c4) // block deduplication!
+                    (2, false),
+                    (3, false), // 2 (c1, c2, c3)
+                    (6, false), // 2 (c1, c2, c3)
+                    (7, false), // 2 (c1, c2, c3)
+                ]),
+            ),
+            dedup_clause(
+                13,
+                None,
+                Clause::new_and([
+                    (0, false), // 3 (c1, c3)
+                    (1, false), // 1 (c0, c3, c4)
+                    (3, false), // 2 (c1, c2, c3)
+                    (4, false), // 3 (c1, c3)
+                    (5, false), // 1 (c0, c3, c4)
+                    (6, false), // 2 (c1, c2, c3)
+                    (7, false), // 2 (c1, c2, c3)
+                ]),
+            ),
+            dedup_clause(
+                14,
+                None,
+                Clause::new_and([
+                    (1, false), // 1 (c0, c3, c4)
+                    (2, false),
+                    (5, false), // 1 (c0, c3, c4)
+                ]),
+            ),
+        ];
+        deduplicate_literal_clauses_0(10, 20, 30, &mut clauses);
+        assert_eq!(
+            vec![
+                DedupClause {
+                    orig_index: 10,
+                    extra_index: None,
+                    clause: Clause {
+                        kind: ClauseKind::And,
+                        literals: vec![(1, false), (2, false), (5, false)]
+                    }
+                },
+                DedupClause {
+                    orig_index: 11,
+                    extra_index: None,
+                    clause: Clause {
+                        kind: ClauseKind::And,
+                        literals: vec![(2, false), (30, false), (31, false)]
+                    }
+                },
+                DedupClause {
+                    orig_index: 12,
+                    extra_index: None,
+                    clause: Clause {
+                        kind: ClauseKind::And,
+                        literals: vec![(1, false), (2, false), (30, false)]
+                    }
+                },
+                DedupClause {
+                    orig_index: 13,
+                    extra_index: None,
+                    clause: Clause {
+                        kind: ClauseKind::And,
+                        literals: vec![(1, false), (5, false), (30, false), (31, false)]
+                    }
+                },
+                DedupClause {
+                    orig_index: 14,
+                    extra_index: None,
+                    clause: Clause {
+                        kind: ClauseKind::And,
+                        literals: vec![(1, false), (2, false), (5, false)]
+                    }
+                },
+                DedupClause {
+                    orig_index: 10,
+                    extra_index: Some(30),
+                    clause: Clause {
+                        kind: ClauseKind::And,
+                        literals: vec![(3, false), (6, false), (7, false)],
+                    },
+                },
+                DedupClause {
+                    orig_index: 10,
+                    extra_index: Some(31),
+                    clause: Clause {
+                        kind: ClauseKind::And,
+                        literals: vec![(0, false), (4, false)]
+                    }
+                }
+            ],
+            clauses,
+        );
     }
 }
