b73d4093f9a50567c2e4f4f4d527c0415c2888a5
matszpk <matszpk@interia.pl>
2023-10-16 19:03:07 +0200

    Next testcase for remove_unused_inputs.

--- src/dedup2_clauses.rs
+++ src/dedup2_clauses.rs
@@ -375,6 +375,19 @@ mod tests {
         bmap.remove_unused_inputs();
         assert_eq!(exp_bmap, bmap);
 
+        let mut bmap = smart_bitmap_from_data(
+            &[3, 4, 6, 9, 11, 12, 14, 15],
+            &[
+                0xbb8811aa006633dd,
+                0xbb8811aa006633dd,
+                0x00ccdd0055bb2211,
+                0x00ccdd0055bb2211,
+            ],
+        );
+        let exp_bmap = smart_bitmap_from_data(&[3, 4, 9, 11, 12, 15], &[0x0cd05b21b81a063d]);
+        bmap.remove_unused_inputs();
+        assert_eq!(exp_bmap, bmap);
+
         let mut bmap = smart_bitmap_from_data(&[3, 4, 6, 9, 11], &[0xaaaa5555]);
         let exp_bmap = smart_bitmap_from_data(&[3, 11], &[0b1001]);
         bmap.remove_unused_inputs();
