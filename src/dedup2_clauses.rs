77bb4da49d097ba6ee52e6145a06557096ef3c7b
matszpk <matszpk@interia.pl>
2023-10-20 18:13:43 +0200

    Update.

--- src/dedup2_clauses.rs
+++ src/dedup2_clauses.rs
@@ -1,21 +1,52 @@
 use gatesim::*;
 
+use std::cmp::Ord;
+use std::collections::HashMap;
+use std::fmt::Debug;
+
 use crate::smart_bitmap::*;
 
+enum DedupRef<T> {
+    Clause(T),
+    ClausePart { clause_index: T, subclause_index: T },
+}
+
 enum Dedup2ClauseBody<T> {
-    Literals {
+    Original {
         // if clause empty - then has been replaced
         clause: Clause<T>,
         // list of option: index - index of literal in clause
         // value - Some(v) - new or old clause index
         used_literals: Vec<Option<T>>,
     },
-    Clause {
+    Replaced {
         new_index: T,
-    }
+    },
 }
 
 struct Dedup2Clause<T> {
-    orig_index: usize,
-    body: Dedup2ClauseBody<T>
+    orig_index: T,
+    body: Dedup2ClauseBody<T>,
+}
+
+impl<T> Dedup2Clause<T>
+where
+    T: Default + Clone + Copy + Debug + Ord + PartialEq + Eq,
+    T: Default + TryFrom<usize>,
+    <T as TryFrom<usize>>::Error: Debug,
+    usize: TryFrom<T>,
+    <usize as TryFrom<T>>::Error: Debug,
+{
+    fn new(orig_index: T, clause: Clause<T>) -> Self {
+        let clause_len = clause.len();
+        Self {
+            orig_index,
+            body: Dedup2ClauseBody::Original {
+                clause,
+                used_literals: vec![None; clause_len],
+            },
+        }
+    }
+
+    fn compare_and_dedup(&mut self, val_map: &mut HashMap<SmartAllValues<T>, T>) {}
 }
