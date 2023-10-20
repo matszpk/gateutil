5068f224e194ad3bedd17c7ccad2b7a6e08906cc
matszpk <matszpk@interia.pl>
2023-10-20 17:37:28 +0200

    Update.

--- src/dedup2_clauses.rs
+++ src/dedup2_clauses.rs
@@ -1,1 +1,21 @@
+use gatesim::*;
+
 use crate::smart_bitmap::*;
+
+enum Dedup2ClauseBody<T> {
+    Literals {
+        // if clause empty - then has been replaced
+        clause: Clause<T>,
+        // list of option: index - index of literal in clause
+        // value - Some(v) - new or old clause index
+        used_literals: Vec<Option<T>>,
+    },
+    Clause {
+        new_index: T,
+    }
+}
+
+struct Dedup2Clause<T> {
+    orig_index: usize,
+    body: Dedup2ClauseBody<T>
+}
