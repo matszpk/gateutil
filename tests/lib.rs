104c3bd645ddee4da72747520e7835aac428445c
matszpk <matszpk@interia.pl>
2024-01-19 14:19:18 +0100

    Add testcase to test_reverse_trans.

--- tests/lib.rs
+++ tests/lib.rs
@@ -111,6 +111,7 @@ fn test_translate_inputs() {
 #[test]
 fn test_reverse_trans() {
     assert_eq!(vec![1, 2, 0, 3], reverse_trans([2, 0, 1, 3]));
+    assert_eq!(vec![2, 4, 1, 0, 3], reverse_trans([3, 2, 0, 4, 1]));
 }
 
 #[test]
