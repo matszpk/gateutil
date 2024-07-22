94d6502b07741041305d0de4840c27d9bcc3986f
matszpk <matszpk@interia.pl>
2024-07-22 08:15:14 +0200

    Add testcase for simple_pipeliner.

--- tests/lib4.rs
+++ tests/lib4.rs
@@ -82,3 +82,42 @@ fn test_min_and_max_depth_list() {
         )
     );
 }
+
+#[test]
+fn test_simple_pipeliner() {
+    assert_eq!(
+        Circuit::new(
+            4,
+            [
+                Gate::new_xor(1, 2),
+                Gate::new_xor(0, 3),
+                Gate::new_nor(1, 2),
+                Gate::new_nor(0, 3),
+                Gate::new_and(4, 5),
+                Gate::new_and(6, 7),
+                Gate::new_nimpl(8, 9),
+                Gate::new_nimpl(9, 10),
+            ],
+            [(11, true)],
+        )
+        .unwrap(),
+        simple_pipeliner(
+            Circuit::new(
+                4,
+                [
+                    Gate::new_xor(1, 2),
+                    Gate::new_xor(0, 3),
+                    Gate::new_nor(1, 2),
+                    Gate::new_nor(0, 3),
+                    Gate::new_and(4, 5),
+                    Gate::new_and(6, 7),
+                    Gate::new_nimpl(8, 9),
+                    Gate::new_nimpl(9, 10),
+                ],
+                [(11, true)],
+            )
+            .unwrap(),
+            5
+        )
+    );
+}
